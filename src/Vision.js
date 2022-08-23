72f75b1b7d2b7872df631836e8964aebd820624d
jdim21 <kd218@protonmail.com>
2022-08-23 19:43:48 -0400

    missed a rename

--- src/Vision.js
+++ src/Vision.js
@@ -11,7 +11,7 @@ const Vision = () => {
     <CssBaseline />
       <Typography color="white" sx={{pt: 5, pb: 1}} variant="h3" fontWeight="bold">VISION</Typography>
       <Typography color="white" sx={{pt: 1, pb: 5}} variant="h6" fontWeight="bold" >
-        Apathetic Apdoge Arsenal aims to support the genesis of the Aptos Network as we believe
+        Wealthy Apdoge Gang aims to support the genesis of the Aptos Network as we believe
         it is the fastest and most secure layer 1 blockchain. NFTs are a crucial part of a growing ecosystem,
         and a doge themed PFP collection is a crucial part to having fun!
         The AAA team are NFT lovers and crypto enthusiasts looking to foster a like-minded community
