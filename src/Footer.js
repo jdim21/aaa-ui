751cd2a77ceb3568e0937f00fe49acdc529c5c63
jdim21 <kd218@protonmail.com>
2022-08-24 17:12:06 -0400

    default footer button links

--- src/Footer.js
+++ src/Footer.js
@@ -4,6 +4,8 @@ import Container from '@mui/material/Container';
 import { CssBaseline, Typography } from "@mui/material";
 import aaaLogo from './aaaLogo.png';
 import wagLogoHidden from './wagLogoHidden.png';
+import discordLogo from './discordLogo.png';
+import twitterLogo from './twitterLogo.png';
 
 const Footer = () => {
   const theme = useTheme();
@@ -12,7 +14,13 @@ const Footer = () => {
     <div style={{backgroundColor: theme.palette.primary.dark}} fontFamily={theme.typography.fontFamily} ref={ref} id={"footer"}>
     <Container fontFamily={theme.typography.fontFamily}>
     <CssBaseline />
+      <a href="https://twitter.com">
+        <img style={{marginBottom:26, marginRight: 26}} width="36" height="32" src={twitterLogo}></img>
+      </a>
       <img width="96" height="96" src={wagLogoHidden}></img>
+      <a href="https://discord.com">
+        <img style={{marginBottom:26, marginLeft: 26}} width="32" height="34" src={discordLogo}></img>
+      </a>
       {/* <Typography color="white" sx={{pt: 5, pb: 1}} variant="body2" fontWeight="bold">Made with love</Typography> */}
     </Container>
     </div>);
