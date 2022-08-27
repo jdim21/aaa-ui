6ac82d212db7ee1ffe79614ce2eabdc6a699d1e0
jdim21 <kd218@protonmail.com>
2022-08-27 09:17:16 -0400

    old name fix

--- src/Vision.js
+++ src/Vision.js
@@ -16,7 +16,7 @@ const Vision = () => {
         Wealthy Apdoge Gang aims to support the genesis of the Aptos Network as we believe
         it is the fastest and most secure layer 1 blockchain. NFTs are a crucial part of a growing ecosystem,
         and a doge themed PFP collection is a crucial part to having fun!
-        The AAA team are NFT lovers and crypto enthusiasts looking to foster a like-minded community
+        The WAG team are NFT lovers and crypto enthusiasts looking to foster a like-minded community
         to explore and support the Aptos Network.
         We're all gonna WAG!
       </Typography>
